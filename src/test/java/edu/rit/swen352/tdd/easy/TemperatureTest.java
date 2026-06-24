5c0bd1d552eb7ff3a47483d4b4e349a6781ba887
sp5938 <sp5938@g.rit.edu>
2026-06-24 11:40:01 -0400

    final changes

--- src/test/java/edu/rit/swen352/tdd/easy/TemperatureTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/TemperatureTest.java
@@ -16,24 +16,28 @@ class TemperatureTest {
     final Temperature temperature = new Temperature(75.0, Temperature.TemperatureUnit.CELSIUS);
     assertNotNull(temperature);
   }
+
   @Test
   @DisplayName("ctor with just value supplied")
   void ctor_2() {
     final Temperature temperature = new Temperature(75.0);
     assertNotNull(temperature);
   }
+
   @Test
   @DisplayName("getValue method returns the temperature value")
   void getValue() {
     final Temperature temperature = new Temperature(75.0, Temperature.TemperatureUnit.CELSIUS);
     assertEquals(0.0, temperature.getValue());
   }
+
   @Test
   @DisplayName("getUnit method returns the temperature unit")
   void getUnit() {
     final Temperature temperature = new Temperature(75.0, Temperature.TemperatureUnit.CELSIUS);
     assertEquals(Temperature.TemperatureUnit.CELSIUS, temperature.getUnit());
   }
+
   @Test
   @DisplayName("convertTo method creates a new Temperature in the new unit")
   void convertTo() {
@@ -42,6 +46,7 @@ class TemperatureTest {
     assertEquals(0.0, convertedTemperature.getValue());
     assertEquals(Temperature.TemperatureUnit.CELSIUS, convertedTemperature.getUnit());
   }
+
   @Test
   @DisplayName("toString method converts returns a human_friendly representation of the temperature")
   void toString_() {
