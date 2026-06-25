3f804dd9664a92ebb66e51eba7e6419fed322753
Bryant Cheng <bc8402@rit.edu>
2026-06-25 07:04:55 -0700

    ctor: no argument constructor defaults to zero : create failing test

--- src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
@@ -2,6 +2,7 @@ package edu.rit.swen352.tdd.easy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import edu.rit.swen352.tdd.easy.SimpleBankAccount;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
  * Test suite for the {@link SimpleBankAccount} component.
  */
 class SimpleBankAccountTest {
+    @Test
+    void ctor_noArgDefaultsBalanceToZero() {
+        SimpleBankAccount account = new SimpleBankAccount();
 
+        assertEquals(0.0f, account.getBalance());
+    }
 }
