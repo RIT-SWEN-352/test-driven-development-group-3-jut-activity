a311655efe98be33f6f173b0ad1d5033dbaf2b5e
Bryant Cheng <bc8402@rit.edu>
2026-07-02 05:11:40 -0700

    toString : format value and units : create passing code

--- src/main/java/edu/rit/swen352/tdd/hard/Measurement.java
+++ src/main/java/edu/rit/swen352/tdd/hard/Measurement.java
@@ -85,9 +85,9 @@ public class Measurement {
     public String getUnits() {
         return units;
     }
+    
     @Override
     public String toString() {
-        assert false : "NYI";
-        return null;
+        return value + units;
     }
 }
